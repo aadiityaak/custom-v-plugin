ec2480b329e5eff846d9526fd421e35601e255fd
aadiityaak <aadiityaak@gmail.com>
2023-04-10 13:37:27 +0700

    Remove some unused sample

--- inc/post-type.php
+++ inc/post-type.php
@@ -31,57 +31,6 @@ function create_post_type_project()
             'menu_icon'   => 'dashicons-building'
         )
     );
-    //KARYAWAN
-    register_post_type(
-        'karyawan',
-        array(
-            'labels' => array(
-                'name' => __('Karyawan'),
-                'add_new'               => __('Tambah Karyawan', 'textdomain'),
-                'singular_name' => __('Karyawan')
-            ),
-            'public' => true,
-            'has_archive' => true,
-            'rewrite' => array('slug' => 'karyawan'),
-            'supports' => array('title', 'thumbnail'),
-            'show_in_rest' => false,
-            'menu_icon'   => 'dashicons-groups'
-        )
-    );
-    //KEUANGAN
-    register_post_type(
-        'keuangan',
-        array(
-            'labels' => array(
-                'name' => __('Keuangan'),
-                'add_new' => __('Tambah Transaksi', 'textdomain'),
-                'singular_name' => __('Keuangan')
-            ),
-            'public' => true,
-            'has_archive' => true,
-            'rewrite' => array('slug' => 'keuangan'),
-            'supports' => array('title', 'author', 'thumbnail'),
-            'show_in_rest' => false,
-            'menu_icon'   => 'dashicons-money'
-        )
-    );
 
-    //invoice
-    register_post_type(
-        'invoice',
-        array(
-            'labels' => array(
-                'name' => __('Invoice'),
-                'add_new' => __('Tambah Invoice', 'textdomain'),
-                'singular_name' => __('Invoice')
-            ),
-            'public' => true,
-            'has_archive' => true,
-            'rewrite' => array('slug' => 'invoice'),
-            'supports' => array('title', 'author', 'thumbnail'),
-            'show_in_rest' => false,
-            'menu_icon'   => 'dashicons-money'
-        )
-    );
 }
 add_action('init', 'create_post_type_project');
